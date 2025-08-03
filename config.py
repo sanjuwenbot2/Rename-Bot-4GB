2a2c654a03c548986021bc491bd57e46df5510d1
sanjuwenbot2 <hubgit222@gmail.com>
2025-08-03 18:24:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002688507790")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002519121603")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002519121603"))
 
 
