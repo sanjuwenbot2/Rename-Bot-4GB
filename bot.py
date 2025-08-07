ff6213e5c714b81884cedeef7e71b15c89ad7290
Alpha-Like <imr@outlook.in>
2025-08-07 18:58:17 +0530

    .

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from plugins.cb_data import app as Client2
 from config import *
 import pyromod
 import pyrogram.utils
+from DA_Koyeb.health import emit_positive_health
 
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
@@ -13,7 +14,7 @@ bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, p
 
 
 
-
+emit_positive_health()
 if STRING_SESSION:
     apps = [Client2,bot]
     for app in apps:
